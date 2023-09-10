8518e9ec0954d7d0802baf18291e25e4f9f86d7a
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2023-09-10 12:50:02 -0700

    add variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,31 @@
+use std::io;
+
 fn main() {
     let x = 5;
     println!("The value of x is: {x}");
-    x = 6;
+    {
+        let x = "hola";
+        println!("Shadowing x {x}");
+    }
+    let x = true;
     println!("The value of x is: {x}");
+
+    // I'm gonna try benefitting from Shadowing in some examples.
+
+    let mut size = String::new();
+
+    io::stdin()
+        .read_line(&mut size)
+        .expect("failed to read message");
+    let size = size.trim();
+
+    let size = size.len();
+
+    println!("Size of the string is: {size}");
+
+    // i guess this was a better example:
+    let spaces = "  ";
+    let spaces = spaces.len();
+
+    println!("There are {spaces} spaces");
 }
