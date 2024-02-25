7a0b094fc82a7bdc89dac3030827483d176be367
Ilia <eilia.abedian@yahoo.com>
2024-02-24 17:18:29 -0800

    init hashmaps

---
+++ starters/collections/src/hashmaps.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+
+pub fn demo() {
+    let reusability = HashMap::<&str, u32>::from(
+        [
+        ("Phone charger", 0),
+        ("Toothbrush", 1),
+        ("Razor and Gillette", 0),
+        ("socks (2 + 1 wearing)", 3),
+        ("happly t-shirt", 7),
+        ("long-sleeve turtle-neck.", 8),
+        ("thick turtle-neck", 6),
+        ("underpants (3 + 1 wearing)", 2),
+        ("plastic bags", 3),
+        ("surface + its charger + its bag", 5),
+        ("Passport", 10),
+        ("flight tickets", 10),
+        ("pair of jeans", 4),
+        ("pair of long-sleeve pants", 4),
+        ]
+        );
+
+    let mut reusability_sorted: Vec<(&&str, &u32)> = reusability.iter().collect();
+    
+    reusability_sorted.sort_by(|a, b| (*a).1.cmp(&(*b).1));
+
+    println!("Pack your items from bottom to top in this order");
+
+    for (item, rank) in reusability_sorted.iter() {
+        println!("{} --- {}", rank, item);
+    }
+}
--- starters/collections/src/main.rs
+++ starters/collections/src/main.rs
@@ -1,5 +1,8 @@
 mod vectors;
+mod hashmaps;
 
 fn main() {
     vectors::demo();
+
+    hashmaps::demo();
 }
