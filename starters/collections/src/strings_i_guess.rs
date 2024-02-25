0c28da51c4a151de013e575d71ceb5f451a4b717
Babak <Abedian.b@gmail.com>
2024-02-25 12:59:36 -0800

    dam strings are hard

--- starters/collections/src/strings_i_guess.rs
+++ starters/collections/src/strings_i_guess.rs
@@ -1,3 +1,5 @@
+use std::{ascii::AsciiExt, ops::Index};
+
 
 pub fn demo() {
     println!("Now we are learning the `String`");
@@ -26,11 +28,50 @@ pub fn demo() {
     let first_name = String::from("Johnathan");
     let last_name = String::from("Blow");
 
+    let _jb_full_name = format!("{first_name} {last_name}");
+
+    // could also get the full name like this
     let jb_full_name = first_name + " " + &last_name;
 
+    // Flexing my dealloc skills.
     drop(last_name);
 
     println!("One of my favorite tech people's name is {jb_full_name}");
+
+    let rough = String::from("A full String collection of \"heterogenous items\" aka. UTF-8 characters aka. just any characters.");
+
+    let _rough_str = &rough[..];
+
+    // I just dereffed the shit out of `rough` by coercing it to give me the &str. Goddammit piece of shit.
+    
+    let a_full = &rough[..6];
+
+    println!("{a_full}");
+
+    // let some_string = "آفرین";
+    // this will give us run-time error!
+    // let _bad_practice = &some_string[0..1];
+
+    // good practice
+    let char_indices = rough.char_indices();
+
+    for (position, char) in char_indices {
+        println!("{} {}", position, char);
+    }
+
+    let chars = rough.chars();
+    for char in chars {
+        print!("{}", char);
+    }
+    println!();
+
+    let bytes = rough.bytes();
+    for byte in bytes {
+        print!("{:^3}", byte);
+    }
+    println!();
+
+    println!("{rough}");
 }
 
 fn normalize_full_name(full_name: &mut String) -> &String {
